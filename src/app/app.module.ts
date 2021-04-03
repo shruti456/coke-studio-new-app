3e76abf682104bcc6dd51f5d48d365f3ba461b95
Shruti Agarwal <shruti.agarwal@exxatsystems.com>
2021-04-03 15:49:14 +0530

    actual code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SideMenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/song.model.ts
@@ -0,0 +1,9 @@
+export class Song {
+    public artists: string[]
+    public cover_image: string;
+    public createdAt: Date;
+    public favourite: boolean;
+    public genre: string;
+    publicid: "1"
+    public title: string;
+}
