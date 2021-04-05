09432e3fea1a6da581d4affd38c3606eefb6ae92
Shruti Agarwal <shruti.agarwal@exxatsystems.com>
2021-04-05 19:19:08 +0530

    Auto-generated commit

--- src/app/models/song.model.ts
+++
@@ -1,9 +0,0 @@
-export class Song {
-    public artists: string[]
-    public cover_image: string;
-    public createdAt: Date;
-    public favourite: boolean;
-    public genre: string;
-    publicid: "1"
-    public title: string;
-}
